83e01f88fe5508a79dc26bc043c00cdebb220776
SirajKarim <msirajakarim437@gmail.com>
2020-04-05 14:59:40 +0500

    javascript Pracctice

---
+++ 2june.js
@@ -0,0 +1,81 @@
+// hold unique values
+// const newSet = new Set(["Aamir", "Pinger", "Aamir"]);
+
+// console.log(newSet);    // Result: Set(2) {"Aamir", "Pinger"}
+
+//==================================================
+//const newSet = new Set();
+// console.log(newSet);
+
+// newSet.add("aamir")
+// newSet.add("pinger")
+// newSet.add("qassim")
+// newSet.delete("qassim")
+// newSet.add("aamir")
+
+// console.log(newSet);    // Result: Set(2) {"Aamir", "Pinger"}
+
+/************************************************** */
+
+
+// const newSet = new Set();
+
+// console.log(newSet);
+
+// newSet.add("aamir")
+// newSet.add("pinger")
+// newSet.add("qassim")
+// newSet.delete("qassim")
+// newSet.add("aamir")
+
+// console.log(newSet)
+
+// for (i of newSet) {
+//     console.log(i)
+// }
+
+// Result:
+// Set(2) {"Aamir", "Pinger"}
+// Aamir
+// Pinger
+
+// try {
+//     aler("hello")   // Spelling mistake in alert to generate error
+// }
+// catch (err) {
+//     console.log("There is some problem " + err)
+// }
+
+// try {
+//     throw " Error(testing try..catch..throw)"
+// }
+// catch (err) {
+//     console.log("There is some problem " + err)
+// }
+
+// class myClass{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+
+//     //class k andar fuction nane ka leye function ka keyword use nhe hita
+//     greetings(){
+//        console.log(this.name,this.age); 
+//     }
+// }
+// // extends is used to inherit class with my class 
+// class secondClass extends myClass {
+//     constructor(name,age){
+//     //super is used to acces parent values that is used in parent class
+//     super(name,age);
+//     }
+// }
+
+// let person=new myClass("siraj",15);
+// console.log(person);
+// console.log(person.greetings());
+
+// let secondClassObject=new secondClass("siraj", 18);
+// secondClassObject.greetings();
+
